346dd38a09f037cd150eaf0ce32dad1bb1146be1
Danny Carcamo <dannycarcamo@icloud.com>
2021-01-04 10:34:00 -0600

    inital files

---
+++ src/app.js
@@ -0,0 +1,25 @@
+/* eslint-disable indent */
+'use strict';
+
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
+
+const app = express();
+
+const morganOption = (process.env.NODE_ENV === 'production')
+  ? 'tiny'
+  : 'common';
+
+app.use(morgan(morganOption));
+app.use(helmet());
+
+app.get('/', (req, res) => {
+    res.send('Hello, boilerplate!');
+});
+
+app.use(cors());
+
+module.exports = app;
